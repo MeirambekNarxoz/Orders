ed25de300164aaf55d0c5854b4e91f275063ece5
MeirambekNarxoz <meirambek.alikulov@narxoz.kz>
2025-04-03 11:15:21 +0500

    add microservice

--- Orders/src/main/java/Make_Orders/client/TypesClient.java
+++ Orders/src/main/java/Make_Orders/client/TypesClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "types-service", url = "http://localhost:8085") // User service on port 8080
+@FeignClient(name = "types-service", url = "http://localhost:8080") // User service on port 8080
 public interface TypesClient {
 
     @GetMapping("/user/{id}")
